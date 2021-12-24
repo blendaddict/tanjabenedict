386da9f3e76c2f0cfaf663c624b428e94bb42f69
Simon <simon.benedict@gmx.de>
2021-12-24 19:15:18 +0100

    first commit

---
+++ src/components/WelcomeHeader.tsx
@@ -0,0 +1,7 @@
+export interface WelcomeHeaderProps {}
+
+export default function WelcomeHeader(props: WelcomeHeaderProps) {
+  return (
+    <h1>Hello, My Name is Tanja Benedict. My Homepage will be here soon...</h1>
+  );
+}
